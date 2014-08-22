993e0a6d4547d686f37ada77a78fb25ff1443daa
k0f <krystof.pesek@gmail.com>
2014-08-22 05:44:15 +0200

    update index file

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
+<?
+`./fetch.sh`;
+
+# Install PSR-0-compatible class autoloader
+spl_autoload_register(function($class){
+	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+
+# Get Markdown class
+use \Michelf\Markdown;
+
+?>
+
 <html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+</head>
 
 <style>
 
 body{
+	font-family: Verdana;
+	font-size: 12px;
 	background-image: url('icoDestor3_sm.png');
 	background-size: width auto;
 }
@@ -18,8 +36,9 @@ body{
 	border: 3px solid;
 	border-color: #2F162B;	
 	margin-left: -220px;
+	margin-bottom: 10px;
 	
-	padding: 0px 20px 20px 20px;
+	padding: 0px 20px 10px 20px;
 
 	-moz-border-radius: 5px;
 	border-radius: 5px;
@@ -27,11 +46,25 @@ body{
 </style>
 <body>
 
-	<div class="frame">
-		<p>
-		<?file_get_contents('/msg.txt');?>
-		</p>
-	</div>
+		<?
+		$src = file_get_contents('www_upkeep/README.md');	 
+		
+		//if ($fp) { 
+ 			$array = explode("~", $src); 
+		//} 
+	
+		use \Michelf\MarkdownExtra;
+		
+		while (list(, $ln) = each($array)){
+		
+			if($ln!=''){	
+			$md = Markdown::defaultTransform($ln);
+			$my_html = Markdown::defaultTransform($ln);
+
+			echo '<div class="frame">'.$my_html.'</div>';
+			}
+		}
+?>
 
 </body>
 </html>
