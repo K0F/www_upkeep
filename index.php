075d6064a8b4593f98d1191215d8ab9ecd991fdd
k0f <krystof.pesek@gmail.com>
2015-08-11 13:12:42 +0200

    AutoCommit @ Tue Aug 11 13:12:42 CEST 2015

--- index.php
+++ index.php
@@ -146,8 +146,8 @@ while (list(, $ln) = each($array)){
                 <div class="footer">
                 <p>
                 Page powered by RaspberryPI, GIT and VIM <br/><br/>
-                last change: <br/>
                 <a href="https://github.com/K0F/www_upkeep">source</a>
+                last change: <br/>
                 <?echo `cd /var/www/ && git log --pretty=format:"%ar" | head -n 1`;?>
                 <br/><br/>                
                 </p>
