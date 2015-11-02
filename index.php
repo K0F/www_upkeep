fe0eaa1b9180497af9f1adbd5c66249ed9874161
k0f <krystof.pesek@gmail.com>
2015-11-02 07:12:22 +0100

    AutoCommit @ Mon Nov  2 07:12:22 CET 2015

--- index.php
+++ index.php
@@ -162,7 +162,7 @@ while (list(, $ln) = each($array)){
                 <a href="https://github.com/K0F/www_upkeep">source</a><br/>
                 <a href="mailto:krystof@pesek.me">contact</a><br/><br/>
                 server up for:
-                <?echo `uptime -p`;?>
+                <?echo `uptime | awk -F',' '{print $1}'`;?>
                 <br/><br/>
                 last change: <br/>
                 <?echo `cd /var/www/ && git log --pretty=format:"%ar" | head -n 1`;?>
